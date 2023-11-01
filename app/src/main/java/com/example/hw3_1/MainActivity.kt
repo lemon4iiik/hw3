f8137448ebeaff43fa19dee0a00cfc2a0f9bddfd
lemon4iiik <musafar2104@mail.ru>
2023-11-01 23:17:19 +0600

    finish

--- app/src/main/java/com/example/hw3_1/MainActivity.kt
+++ app/src/main/java/com/example/hw3_1/MainActivity.kt
@@ -24,21 +24,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun foodListItems(){
-        foodList.add(Food(R.drawable.pizza1,"chicken Pizza"))
-        foodList.add(Food(R.drawable.pizza2,"Beef Chicken"))
-        foodList.add(Food(R.drawable.noodles1,"normal Pizza"))
-        foodList.add(Food(R.drawable.noodles2,"sweet Chicken"))
+        foodList.add(Food(R.drawable.pizza1,"Pepperoni"))
+        foodList.add(Food(R.drawable.pizza2,"Firmennyi"))
+        foodList.add(Food(R.drawable.pizza3,"Gripbnoi"))
+        foodList.add(Food(R.drawable.noodles1,"makarowki"))
+        foodList.add(Food(R.drawable.noodles2,"Domawnyi Noodles"))
         foodList.add(Food(R.drawable.paneer,"Mini Pizza"))
-        foodList.add(Food(R.drawable.pasta,"Extra Chicken"))
-        foodList.add(Food(R.drawable.pizza_spagethi,"meat Pizza"))
-        foodList.add(Food(R.drawable.pizza3,"vegez Chicken"))
-        foodList.add(Food(R.drawable.pizza1,"chicken Pizza"))
-        foodList.add(Food(R.drawable.pizza2,"Beef Chicken"))
-        foodList.add(Food(R.drawable.noodles1,"normal Pizza"))
-        foodList.add(Food(R.drawable.noodles2,"sweet Chicken"))
-        foodList.add(Food(R.drawable.paneer,"Mini Pizza"))
-        foodList.add(Food(R.drawable.pasta,"Extra Chicken"))
-        foodList.add(Food(R.drawable.pizza_spagethi,"meat Pizza"))
-        foodList.add(Food(R.drawable.pizza3,"vegez Chicken"))
+        foodList.add(Food(R.drawable.paste,"suwi"))
+        foodList.add(Food(R.drawable.pizza_spagethi,"pizza_spagethi"))
+
+//        foodList.add(Food(R.drawable.pizza1,"chicken Pizza"))
+//        foodList.add(Food(R.drawable.pizza2,"Beef Chicken"))
+//        foodList.add(Food(R.drawable.noodles1,"normal Pizza"))
+//        foodList.add(Food(R.drawable.noodles2,"sweet Chicken"))
+//        foodList.add(Food(R.drawable.paneer,"Mini Pizza"))
+//        foodList.add(Food(R.drawable.pasta,"Extra Chicken"))
+//        foodList.add(Food(R.drawable.pizza_spagethi,"meat Pizza"))
+//        foodList.add(Food(R.drawable.pizza3,"vegez Chicken"))
     }
 }
